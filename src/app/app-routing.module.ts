d3937a94d58d4934886979b6542108742601edc7
stepforwardtech <jrstrayhorn@gmail.com>
2019-04-19 16:43:31 -0400

    adding structure and flexbox css for overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { SignupComponent } from './auth/signup/signup.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
