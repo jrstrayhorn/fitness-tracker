21dc1f91dd7204cecb75f9e06c15aff1bea9364d
stepforwardtech <jrstrayhorn@gmail.com>
2019-05-30 07:30:31 -0400

    done with adding "exit" and "continue" options

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training/stop-training.component';
 
 @Component({
@@ -8,28 +8,37 @@ import { StopTrainingComponent } from './stop-training/stop-training.component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
+    this.startOrResumeTimer();
+  }
+
+  stop() {
+    clearInterval(this.timer);
+    let dialogRef: MatDialogRef<
+      StopTrainingComponent,
+      boolean
+    > = this.dialog.open(StopTrainingComponent, {
+      data: { progress: this.progress }
+    });
+
+    dialogRef.afterClosed().subscribe(isExitConfirmed => {
+      if (isExitConfirmed) this.trainingExit.emit();
+      this.startOrResumeTimer();
+    });
+  }
+
+  startOrResumeTimer() {
     this.timer = <any>setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
     }, 1000);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-    const dialogRef = this.dialog.open(StopTrainingComponent, {
-      data: { progress: this.progress }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
-    });
   }
 }
