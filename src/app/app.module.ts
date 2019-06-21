b355daf7f177d621c066242616f73844739b861f
stepforwardtech <jrstrayhorn@gmail.com>
2019-06-21 08:12:16 -0400

    done with splitting the app into modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,42 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-import { StopTrainingComponent } from './training/current-training/stop-training/stop-training.component';
 import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
 import { UIService } from './shared/ui.service';
+import { AuthModule } from './auth/auth.module';
+import { TrainingModule } from './training/training.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
-    PastTrainingsComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent,
-    StopTrainingComponent
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
@@ -45,14 +31,12 @@ import { UIService } from './shared/ui.service';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MaterialModule,
-    FormsModule,
-    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AuthModule,
+    TrainingModule
   ],
   providers: [AuthService, TrainingService, UIService], // this will register service as singleton will only have one instance used throughout app
-  bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent] // not by selector or routing, like a dynamic dialog
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './../material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
+
+// should only add providers on app.module so they can be available app wide or as a singleton
+// if you have any providers that are specific to just this module they can be placed
+// as a provider here
+@NgModule({
+  declarations: [SignupComponent, LoginComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule
+  ],
+  exports: []
+})
+export class AuthModule {}
