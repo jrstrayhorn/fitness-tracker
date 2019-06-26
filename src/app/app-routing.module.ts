31cb98755fb25ce540bd18e3100fa83b752e6c4a
stepforwardtech <jrstrayhorn@gmail.com>
2019-06-26 07:51:14 -0400

    done with splitting up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
-import { SignupComponent } from './auth/signup/signup.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
+
   { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] }
 ];
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 // should only add providers on app.module so they can be available app wide or as a singleton
 // if you have any providers that are specific to just this module they can be placed
 // as a provider here
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [SharedModule, AngularFireAuthModule]
+  imports: [SharedModule, AngularFireAuthModule, AuthRoutingModule]
 })
 export class AuthModule {}
